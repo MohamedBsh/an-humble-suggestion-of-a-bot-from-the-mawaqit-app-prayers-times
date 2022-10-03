f8e0778c719322178a2aa5108c0419f7c722bc3c
MohamedBsh <mohamedamine.bousahih.partner@decathlon.com>
2022-10-04 00:02:21 +0200

    feat: first test

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+from typing import Any, List
+from prayers_times import PrayersTimesPage
+
+
+class Parser:
+    def __init__(self, arg_list: List[Any] = []):
+        super().__init__(arg_list)
+        self.add_job_specific_args()
+
+    def add_job_specific_args(self) -> None:
+        self.add_argument(
+            "--url",
+            type=str,
+            default="",
+            required=True,
+            help=("URL - Website Masjid of Paris"),
+        )
+
+
+def main(arg_list: List[Any] = []) -> None:
+    parser = argparse.ArgumentParser(description='This is the main.py script for test')
+    parser.add_argument('--url',action='store',dest='url',default=None,help='<Required> url link',required=True)
+    results = parser.parse_args()
+    url = results.url
+    time_prayers_of_the_day = PrayersTimesPage(url)._get_times_prayers_of_the_day()
+    print(time_prayers_of_the_day)
+
+if __name__ == "__main__":
+    main()
