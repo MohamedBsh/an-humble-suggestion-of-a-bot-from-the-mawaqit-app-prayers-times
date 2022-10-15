92f4eac1241c98948dc3de8708d1e659a40c57a0
MohamedBsh <mohamedamine.bousahih.partner@decathlon.com>
2022-10-15 21:21:29 +0200

    feat: test pad_month from source file

--- src/transform.py
+++ src/transform.py
@@ -1,2 +1,3 @@
-def transform(prayers: dict):
-    pass
+def pad_month(data):
+    month_padded = dict((key, key.zfill(2)) for key in data["calendar"][0])
+    return dict([(month_padded.get(k), v) for k, v in data["calendar"][0].items()])
