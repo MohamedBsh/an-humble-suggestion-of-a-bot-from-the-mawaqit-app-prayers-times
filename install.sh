a92df942cb5ab6e4601929cf0ec0a89f3969d552
MohamedBsh <m.bousahih@gmail.com>
2022-11-19 17:17:02 +0100

    feat: install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-python3 -m pipenv install
-python3 -m pipenv install --dev
+export AIRFLOW_HOME=$(pwd)/airflow
+export PYTHONPATH=$(pwd)
+
+pip install 'apache-airflow[postgres]'
+python3 -m pipenv install -r requirements.txt
 python3 -m pipenv shell
