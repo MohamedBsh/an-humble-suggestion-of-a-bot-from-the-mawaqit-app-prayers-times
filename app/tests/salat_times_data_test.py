8b3ae489368c3e9ea194ce96cbbfbcbe215efec2
MohamedBsh <m.bousahih@gmail.com>
2023-03-08 02:11:58 +0100

    feat: check duplicate test on data

--- app/tests/salat_times_data_test.py
+++ app/tests/salat_times_data_test.py
@@ -1,9 +1,13 @@
 from app.model import Connection, SalatTimes
 
-session = Connection.get_session()
-num_duplicates = session.query(SalatTimes).distinct(SalatTimes.day, SalatTimes.name_prayers, SalatTimes.times_prayer,
-                                                    SalatTimes.iqama_difference, SalatTimes.time_jumua_1,
-                                                    SalatTimes.time_jumua_2).count()
 
-print(f"Number of duplicate rows: {session.query(SalatTimes).count() - num_duplicates}")
-session.close()
+def main(db_connection: str):
+    connection = Connection(db_connection)
+    session = connection.get_session()
+    num_duplicates = session.query(SalatTimes).distinct(SalatTimes.day, SalatTimes.name_prayers,
+                                                        SalatTimes.times_prayer,
+                                                        SalatTimes.iqama_difference, SalatTimes.time_jumua_1,
+                                                        SalatTimes.time_jumua_2).count()
+
+    print(f"Number of duplicate rows: {session.query(SalatTimes).count() - num_duplicates}")
+    session.close()
